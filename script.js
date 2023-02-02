e3d8bd448e1953a94a8a93072179cd2fc0f47bac
Michael Sullivan <sullivanmichael344@gmail.com>
2023-02-01 20:57:20 -0500

    Make function to play a single round

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ let playerSelection;
 function getComputerChoice(){
    let result=Math.floor(Math.random() * 3) + 1;
 
-   if (result==1){
-    return "Rock"
-   } else if (result==2){
-    return "Paper"
+   if (result == 1){
+    return "rock"
+   } else if (result == 2){
+    return "paper"
    } else{
-    return "Scissors"
+    return "scissors"
    }
 }
 
-computerSelection=getComputerChoice();
-console.log(computerSelection);
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return `It's a tie! You both chose ${playerSelection}`
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }
+}
 
+playerSelection=prompt("Choose rock, paper or scissors").toLowerCase();
+computerSelection=getComputerChoice();
+console.log(playRound(playerSelection,computerSelection))
