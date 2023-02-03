4c305ee741f2c810abb1e1cd300cfb1ee03883bf
Michael Sullivan <sullivanmichael344@gmail.com>
2023-02-03 08:13:25 -0500

    Make game function to play 5 rounds
    Added score counter to playRound(). Made game() to call playRound()
    5 times and report winner at the end.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let computerSelection;
 let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
    let result=Math.floor(Math.random() * 3) + 1;
@@ -15,14 +17,34 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return `It's a tie! You both chose ${playerSelection}`
+        return `It's a tie! You both chose ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore= ++playerScore;
         return `You win! ${playerSelection} beats ${computerSelection}!`
     } else {
+        computerScore= ++computerScore;
         return `You lose! ${computerSelection} beats ${playerSelection}!`
     }
 }
 
-playerSelection=prompt("Choose rock, paper or scissors").toLowerCase();
-computerSelection=getComputerChoice();
-console.log(playRound(playerSelection,computerSelection))
+function game(){
+    for(let i = 0; i < 5; i++) {
+        playerSelection=prompt("Choose rock, paper or scissors").toLowerCase();
+        computerSelection=getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection))
+        if (i == 4){
+            if (playerScore == computerScore){
+                console.log(`You won ${playerScore} rounds and the computer won ${computerScore} rounds, It's a tie!`)
+            } else if(playerScore<computerScore){
+                console.log(`You won ${playerScore} rounds and the computer won ${computerScore} rounds, you lose!`)
+            } else{
+                console.log(`You won ${playerScore} rounds and the computer won ${computerScore} rounds, you win!`)
+            }
+            
+        }
+    }
+    
+}
+
+game();
+
